55e141637a7758167cb12ce6f6fd8482da79a0ff
mamat <mamat@NU14A1.mshome.net>
2023-06-22 20:56:19 +0530

    newly added param_demo project

---
+++ src/test/java/day20/Demo1.java
@@ -0,0 +1,17 @@
+package day20;
+
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class Demo1 {
+	
+   @Parameters({"city" })
+	@Test
+	public void testA(String city)
+	{
+		Reporter.log("testA...."+city, true);
+	}
+}
